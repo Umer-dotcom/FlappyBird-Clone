1787663ff854f4f28a7abd6c110e74b5c4df5ade
Umer-dotcom <76922950+Umer-dotcom@users.noreply.github.com>
2021-08-03 20:50:24 +0500

    Game Manager Added!
    Scenery Scroller Script + Scenery Continuation :)

---
+++ FlappyBird-Clone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject scenerayPrefab;
+    [SerializeField] GameObject sceneToBeDestroyed;
+    [SerializeField] GameObject sceneNewlyInstantiated;
+
+    [SerializeField] float scrollingSpeed = 40f;
+    [SerializeField] float xPosToDestroyScenery = -12.5f;
+    [SerializeField] float xPosToCreateScenery = -6.8f;
+    [SerializeField] Vector3 newScenePosition;
+    [SerializeField] Vector3 oldScenePosition;
+
+    private void Awake()
+    {
+        sceneNewlyInstantiated = Instantiate(scenerayPrefab, Vector3.zero, Quaternion.identity); //Instantiating Started Screen
+        sceneToBeDestroyed = sceneNewlyInstantiated;    //At first both will point to same scenery
+        sceneNewlyInstantiated.GetComponent<SceneryScroller>().setScrollingSpeed(scrollingSpeed);   //Setting speed of scrolling
+    }
+
+    private void Update()
+    {
+        newScenePosition = sceneNewlyInstantiated.GetComponent<Transform>().position;
+        oldScenePosition = sceneToBeDestroyed.GetComponent<Transform>().position;
+
+        checkCreateScenery();
+        checkDestroyScenery();
+
+    }
+
+    private void checkCreateScenery()
+    {
+        if(newScenePosition.x <= xPosToCreateScenery)
+        {
+            //Create new scenery at x = 12
+            createNewScenery();
+        }
+    }
+
+    private void createNewScenery()
+    {
+        sceneToBeDestroyed = sceneNewlyInstantiated;
+        sceneNewlyInstantiated = Instantiate(scenerayPrefab, new Vector2(12, 0), Quaternion.identity);
+        sceneNewlyInstantiated.GetComponent<SceneryScroller>().setScrollingSpeed(scrollingSpeed);
+    }
+
+    private void checkDestroyScenery()
+    {
+        if(oldScenePosition.x <= xPosToDestroyScenery)
+        {
+            Destroy(sceneToBeDestroyed);
+            sceneToBeDestroyed = sceneNewlyInstantiated;
+        }
+    }
+
+}
---
+++ FlappyBird-Clone/Assets/Scripts/SceneryScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SceneryScroller : MonoBehaviour
+{
+    [SerializeField] float scrollingSpeed;
+    [SerializeField] Rigidbody2D rgb;
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rgb.velocity = Vector2.left * scrollingSpeed * Time.deltaTime;  //Using velocity to move it
+    }
+
+    public void setScrollingSpeed(float speed)
+    {
+        scrollingSpeed = speed;
+    }
+
+}
